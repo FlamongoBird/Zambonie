0ca52ff95bb0b7ccea82a139b6bd6d4439cc36ca
FlamongoBird <jcfreebuilder@gmail.com>
2024-02-10 12:27:57 -0600

    new title

--- game/title.nim
+++ game/title.nim
@@ -1,8 +1,22 @@
 import "../terminal/terminal"
 import std/terminal
+import strutils
 
-const title = @["Boogie Woogie! Less GO!!!", "", "Press Any Key to Continue"]
+const title = """
+
+  _____   _    __  __ ____   ___  _   _ ___ _____ 
+ |__  /  / \  |  \/  | __ ) / _ \| \ | |_ _| ____|
+   / /  / _ \ | |\/| |  _ \| | | |  \| || ||  _|  
+  / /_ / ___ \| |  | | |_) | |_| | |\  || || |___ 
+ /____/_/   \_\_|  |_|____/ \___/|_| \_|___|_____|
+                                                  
+                                                  
+                                                  
+             Press Any Key to Continue
+""".split("\n")
+
 
 proc showTitle*() =
     display(title)
     discard getch()
+    eraseScreen()
