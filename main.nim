5be00e51eee91882bfd67a6c693668e1c26cbf54
FlamongoBird <jonathancalhoon@gmail.com>
2024-02-09 20:44:42 -0600

    basics are working

--- main.nim
+++ main.nim
@@ -12,21 +12,37 @@ var
     y = 5
 
 while true:
+    #eraseScreen()
     dungeon_map[y][x] = 69
     printDungeonMap(dungeon_map)
+    dungeon_map[y][x] = 0
     var keypress = getch()
+
+    var temp_x = 0
+    var temp_y = 0
     
     case keypress:
         of 'e':
             break
         of 'h':
-            x -= 1
+            temp_x = -1
         of 'j':
-            y += 1
+            temp_y = 1
         of 'k':
-            y -= 1
+            temp_y = -1
         of 'l':
-            x += 1
+            temp_x = 1
         else:
             # TODO: actually do something here
             continue
+    
+    var spot = dungeon_map[temp_y+y][temp_x+x]
+
+    case spot:
+        of 0:
+            x += temp_x
+            y += temp_y
+        else:
+            continue
+
+
